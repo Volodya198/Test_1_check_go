4b78351259a0bc170eebc3df23c1716b4d65bc29
Volodya <saisile73@gmail.com>
2025-10-04 17:09:20 +0300

    Update backend with CORS and new API

--- main.go
+++ main.go
@@ -8,6 +8,16 @@ import (
 
 func main() {
 	r := gin.Default()
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
 
 	r.GET("/api/hello", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
